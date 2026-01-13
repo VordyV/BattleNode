845cb2f565056c72141f56ac2e1dc255591d7448
vordy <vorklab@outlook.com>
2026-01-13 19:03:10 +0300

    Added the shared_data property; added an init method for initializing Redis

--- battlenode/new_process.py
+++ battlenode/new_process.py
@@ -1,15 +1,30 @@
 import queue
 import asyncio
 from typing import Callable
+import redis.asyncio as redis
+import os
+from ._shared_data import SharedData
 
 class NewProcess:
 
-    def __init__(self, queue: queue.Queue, app_config: dict):
+    def __init__(self, queue: queue.Queue, app_config: dict, app_name: str):
         self.__queue = queue
         self.__app_config = app_config
+        self.__app_name = app_name
+        self.__redis: redis.Redis | None = None
+        self.__shared_data: SharedData | None = None
+
+    def init(self):
+        self.__redis = redis.Redis(host=os.getenv("BN_REDIS_HOST"), port=int(os.getenv("BN_REDIS_PORT")), db=int(os.getenv("BN_REDIS_NAME")), password=os.getenv("BN_REDIS_PASSWORD"))
+        self.__shared_data = SharedData(redis=self.__redis, name=self.__app_name)
 
     @property
-    def config(self): return self.__app_config
+    def config(self) -> dict:
+        return self.__app_config
+
+    @property
+    def shared_data(self) -> SharedData:
+        return self.__shared_data
 
     def debug(self, text: str): self.__queue.put(("debug", text))
     def info(self, text: str): self.__queue.put(("info", text))

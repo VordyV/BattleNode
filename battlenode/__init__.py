121b5e784750e419a9faef09b16aaa6579780eba
vordy <vorklab@outlook.com>
2026-01-13 19:07:59 +0300

    Changed to version 0.2.0; Added the opengsq module

--- battlenode/__init__.py
+++ battlenode/__init__.py
@@ -4,4 +4,4 @@ from ._events import EventHub, EventCollection, EventData
 from ._database import init_database, close_database
 from .new_process import NewProcess
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"

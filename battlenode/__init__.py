c301b1d0d5e78b2022f4e1d36cc64488fc46b569
vordy <vorklab@outlook.com>
2026-01-29 22:26:19 +0300

    changed version to 0.3.0

--- battlenode/__init__.py
+++ battlenode/__init__.py
@@ -3,5 +3,6 @@ from ._plugin import BasePlugin
 from ._events import EventHub, EventCollection, EventData
 from ._database import init_database, close_database
 from .new_process import NewProcess
+from ._commands import CommandCollection
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"

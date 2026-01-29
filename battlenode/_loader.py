b619363f41cdac62ce7ebfc74fa4b4c6d61bce6e
vordy <vorklab@outlook.com>
2026-01-29 22:16:11 +0300

    Added property to retrieve an instance of all loaded plugins; added exit codes for plugin shutdown; added the shutdown method; added handling of command registration and removal from plugins; added the init_plugin method

--- battlenode/_loader.py
+++ battlenode/_loader.py
@@ -11,11 +11,13 @@ from ._events import EventHub, EventCollection
 from ._database import init_database, close_database
 from ._process import ProcessSupervisor
 from ._shared_data import SharedData
+from ._commands import CommandCollection
 import inspect
 import packaging.version
 import packaging.specifiers
 import battlenode
 import pydantic
+from pathlib import Path
 
 class Loader:
 
@@ -35,6 +37,10 @@ class Loader:
         plugin = self.__plugins[name]
         return plugin
 
+    @property
+    def plugins(self) -> dict[str, Plugin]:
+        return self.__plugins
+
     async def _on_event_prodis_submitted(self, event):
         self.__events.emit_future(f"{event.job_id}.process.submitted", event)
 
@@ -42,18 +48,21 @@ class Loader:
         self.__events.emit_future(f"{event.job_id}.process.executes", event)
 
         plugin = self.__plugins[event.job_id]
-        if plugin.instance.run_as_process and plugin.instance.process_target and plugin.status == PluginStatuses.RUNNING:
-            await self.shutdown_plugin(plugin)
+        if plugin.instance and plugin.instance.run_as_process and plugin.instance.process_target and plugin.status == PluginStatuses.RUNNING:
+            await self.shutdown_plugin(plugin, "child_process_terminated")
 
     async def _on_event_prodis_error(self, event):
         self.__events.emit_future(f"{event.job_id}.process.error", event)
         
         plugin = self.__plugins[event.job_id]
         if plugin.instance.run_as_process and plugin.instance.process_target and plugin.status == PluginStatuses.RUNNING:
-            await self.shutdown_plugin(plugin)
+            await self.shutdown_plugin(plugin, "error_child_process")
 
     async def init(self):
         await self.__prodis.init()
+
+    async def shutdown(self):
+        await self.__prodis.shutdown()
 
     async def load_plugins(self):
         if len(self.__plugins) > 1: raise LoaderReLoadPluginException("Plugins have already been loaded. Use reload methods")
@@ -75,14 +84,14 @@ class Loader:
         return {pl.name: pl.app for pl in self.__plugins.values()}
 
     async def init_database(self, apps: dict):
-        await init_database(apps)
+        await init_database(apps, self.__battlenode.config)
         self.__events.emit_future(f"{battlenode.__name__}.database.init")
 
     async def shutdown_plugins(self):
         tasks = []
         for plugin in self.__plugins.values():
             if plugin.status != PluginStatuses.RUNNING: continue
-            tasks.append(asyncio.create_task(self.shutdown_plugin(plugin)))
+            tasks.append(asyncio.create_task(self.shutdown_plugin(plugin, "general_shutdown")))
 
         results = await asyncio.gather(*tasks, return_exceptions=True)
 
@@ -92,6 +101,8 @@ class Loader:
                 tb = ''.join(traceback.format_exception(type(result), result, result.__traceback__))
                 plugin.logger.exception("plugin didn't shut down properly: {error}\n{tb}", error=result, tb=tb)
                 #print("SHUTDOWN ERROR", plugin.name, result)
+
+        self.__plugins.clear()
 
         await close_database()
 
@@ -125,25 +136,19 @@ class Loader:
             await self.__set_status(plugin, PluginStatuses.ERROR)
             raise
 
-        try:
-            await self.__set_status(plugin, PluginStatuses.INITIALIZING)
-            await self._init_pl(plugin)
-            await self.__set_status(plugin, PluginStatuses.RUNNING)
-            await self.__events.emit_async(f"{plugin.name}.init")
-        except:
-            await self.__set_status(plugin, PluginStatuses.ERROR)
-            raise
+        await self.init_plugin(plugin)
 
-    async def shutdown_plugin(self, plugin: Plugin):
+    async def shutdown_plugin(self, plugin: Plugin, exitcode: str):
         if plugin.status != PluginStatuses.RUNNING: raise LoaderShutNonWorkException(f"Plugin {plugin.name} is inoperable and cannot be turned off")
         try:
             await self.__set_status(plugin, PluginStatuses.STOPPING)
-            await self._shutdown_plugin(plugin)
+            await self._shutdown_plugin(plugin, exitcode)
         except:
             await self.__set_status(plugin, PluginStatuses.ERROR)
             raise
 
-    async def _shutdown_plugin(self, plugin: Plugin):
+    async def _shutdown_plugin(self, plugin: Plugin, exitcode: str):
+        plugin._set_exitcode(exitcode)
         await self.__events.emit_async(f"{plugin.name}.shutdown")
 
         if plugin.instance.run_as_process and plugin.instance.process_target and plugin.status.RUNNING:
@@ -155,11 +160,17 @@ class Loader:
             if "." in e["event"]: self.__events.off(e["event"], getattr(plugin.instance, e["func"]))
             else: self.__events.off(f"{plugin.name}.{e["event"]}", getattr(plugin.instance, e["func"]))
 
+        _commands = plugin.instance.commands.get()
+        for c in _commands:
+            self.__battlenode.command_shell.unregister(c["command"])
+
+        plugin._del_instance()
+
     async def reload_plugin(self, plugin: Plugin):
         if plugin.status != PluginStatuses.RUNNING: raise LoaderShutNonWorkException(f"Plugin {plugin.name} is inoperable and cannot be turned off")
         try:
             await self.__set_status(plugin, PluginStatuses.RESTARTING)
-            await self._shutdown_plugin(plugin)
+            await self._shutdown_plugin(plugin, "reload")
             loop = asyncio.get_event_loop()
             await loop.run_in_executor(
                 None,
@@ -203,13 +214,17 @@ class Loader:
     def _pre_init_pl(self, plugin: Plugin, module: object):
         cls = self.__get_cls_pl(plugin.name, module)
 
+        if not issubclass(cls, BasePlugin): raise LoaderNotSubClsBaseException(f"Plugin '{plugin.name}' class is not a subclass of BasePlugin")
+
         config = None
         if cls.Config and issubclass(cls.Config, pydantic.BaseModel):
             config = self.__battlenode.configurator.get_section(plugin.name, cls.Config)
 
         instance = cls(self.__battlenode, config, plugin.logger, SharedData(self.__battlenode.redis, plugin.name), self.__battlenode.scheduler)
 
-        if instance is None or not isinstance(instance, EventCollection): LoaderNoEventInstException(f"Plugin {plugin.name} does not contain an instance of the Events class")
+        if instance.events is None or not isinstance(instance.events, EventCollection): raise LoaderNoEventInstException(f"Plugin '{plugin.name}' does not contain an instance of the Events class")
+        if instance.commands is None or not isinstance(instance.commands, CommandCollection): raise LoaderNoCommandInstException(f"Plugin '{plugin.name}' does not contain an instance of the Commands class")
+        #if instance is None or not isinstance(instance, CommandCollection): LoaderNoCommandInstException(f"Plugin {plugin.name} does not contain an instance of the Commands class")
         #"minsize": app.get("minsize", os.getenv("BN_DATABASE_MINSIZE")),
         #"maxsize": app.get("maxsize", os.getenv("BN_DATABASE_MAXSIZE")),
         #"connect_timeout": app.get("connect_timeout", os.getenv("BN_DATABASE_TIMEOUT")),
@@ -222,17 +237,21 @@ class Loader:
             if "." in e["event"]: self.__events.on(e["event"], getattr(instance, e["func"]))
             else: self.__events.on(f"{plugin.name}.{e["event"]}", getattr(instance, e["func"]))
 
+        _commands = instance.commands.get()
+        for c in _commands:
+            self.__battlenode.command_shell.register(c["command"], getattr(instance, c["func"]), c["desc"])
+
         if cls.run_as_process and cls.process_target:
             self.__prodis.add_process(plugin, cls.process_target)
 
     async def _init_pl(self, plugin: Plugin):
         spec_ver_pl = packaging.specifiers.SpecifierSet(plugin.meta.requires_battlenode)
         ver_bn = packaging.version.parse(battlenode.__version__)
-        if ver_bn not in spec_ver_pl: raise LoaderInvalidVerSpecException(f"Plugin {plugin.name} cannot run on this version {battlenode.__version__}. Possible versions: {plugin.meta.requires_battlenode}")
+        if ver_bn not in spec_ver_pl: raise LoaderInvalidVerSpecException(f"Plugin '{plugin.name}' cannot run on this version '{battlenode.__version__}'. Possible versions: '{plugin.meta.requires_battlenode}'")
         self.__check_dependencies_pl(plugin)
 
         if plugin.instance.run_as_process and plugin.instance.process_target:
-            self.__prodis.run_process(plugin.name, plugin.instance.config.dict())
+            self.__prodis.run_process(plugin.name, plugin.instance.config.dict(), self.__battlenode.config.dict())
 
     def __check_dependencies_pl(self, plugin: Plugin):
         for name, spec_ver in plugin.meta.dependencies.items():
@@ -242,10 +261,10 @@ class Loader:
                 spec_ver_pl = packaging.specifiers.SpecifierSet(spec_ver)
                 if dep_ver not in spec_ver_pl: raise
             except:
-                raise LoaderNoDepPluginException(f"Plugin {plugin.name} requires {name} with version {spec_ver}")
+                raise LoaderNoDepPluginException(f"Plugin '{plugin.name}' requires '{name}' with version '{spec_ver}'")
 
     def _add_new_pl(self, module_name: str) -> Plugin:
-        if module_name in self.__plugins: raise LoaderPluginExistsException(f"Plugin {module_name} already added")
+        if module_name in self.__plugins: raise LoaderPluginExistsException(f"Plugin '{module_name}' already added")
         plugin = Plugin(module_name)
         self.__plugins[module_name] = plugin
         return plugin
@@ -253,3 +272,15 @@ class Loader:
     def _get_all_plugin(self) -> list[str]:
         return self.__battlenode.config.plugins
 
+    async def init_plugin(self, plugin: Plugin, pre_init: bool = False):
+        if plugin.status != PluginStatuses.STOPPED and plugin.status != PluginStatuses.ERROR and plugin.status != PluginStatuses.LOADED: raise LoaderShutNonWorkException(f"Plugin '{plugin.name}' must be turned off")
+        try:
+            await self.__set_status(plugin, PluginStatuses.INITIALIZING)
+            if pre_init: self._pre_init_pl(plugin, plugin.module)
+            await self._init_pl(plugin)
+            await self.__set_status(plugin, PluginStatuses.RUNNING)
+            plugin._set_exitcode(None)
+            await self.__events.emit_async(f"{plugin.name}.init")
+        except:
+            await self.__set_status(plugin, PluginStatuses.ERROR)
+            raise

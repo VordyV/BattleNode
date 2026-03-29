ba45f522419e6eb2412c7434d922774da415d071
vordy <vorklab@outlook.com>
2026-03-29 13:34:21 +0300

    Refactored logging methods; removed unnecessary string conversion from data type in the publish method; added the start method

--- battlenode/new_process.py
+++ battlenode/new_process.py
@@ -6,20 +6,38 @@ import os
 from ._shared_data import SharedData
 import json
 from ._config import Config
+from enum import Enum
+import traceback
+from battlenode import init_database
 
 class NewProcess:
 
-    def __init__(self, queue: queue.Queue, config: Config, app_config: Config, app_name: str):
+    def __init__(self, queue: queue.Queue, config: Config, app_config: Config, app_name: str, db_apps: dict | None = None):
         self.__queue = queue
         self.__config = config
         self.__app_config = app_config
         self.__app_name = app_name
+        self.__db_apps = db_apps
         self.__redis: redis.Redis | None = None
         self.__shared_data: SharedData | None = None
 
     def init(self):
+        if self.__redis is not None: raise Exception("Initialization already performed")
+
         self.__redis = redis.Redis(host=self.__app_config.get("redis_host"), port=self.__app_config.get("redis_port"), db=self.__app_config.get("redis_name"), password=self.__app_config.get("redis_password"))
         self.__shared_data = SharedData(redis=self.__redis, name=self.__app_name)
+
+    async def __main(self, handler: Callable):
+        if self.__db_apps:
+            await init_database(apps=self.__db_apps, app_config=self.app_config)
+        self.init()
+        await handler(self)
+
+    def start(self, handler: Callable):
+        try:
+            asyncio.run(self.__main(handler))
+        except Exception as e:
+            self.error(str(e), traceback.format_exc())
 
     @property
     def app_config(self) -> Config:
@@ -37,20 +55,18 @@ class NewProcess:
     def redis(self) -> redis.Redis:
         return self.__redis
 
-    def debug(self, text: str): self.__queue.put(("debug", text))
-    def info(self, text: str): self.__queue.put(("info", text))
-    def error(self, text: str): self.__queue.put(("error", text))
-    def warning(self, text: str): self.__queue.put(("warning", text))
+    def log(self, level: str, text: str, **kwargs): self.__queue.put({"level": level, "text": text, **kwargs})
+
+    def debug(self, text: str): self.log("debug", text)
+    def info(self, text: str): self.log("info", text)
+    def error(self, text: str, tb: str): self.log("error", text, traceback=tb)
+    def warning(self, text: str): self.log("warning", text)
+    def exitcode(self, text: str): self.log("exitcode", text)
 
     async def publish(self, event_name: str, arg: object = None) -> None:
-        try:
-            event_data = json.dumps(arg) if arg else None
-        except:
-            event_data = None
-
         data = {
             "sender": f"p_{self.__app_name}",
-            "data": event_data
+            "data": arg
         }
         await self.__redis.publish(event_name, json.dumps(data))
 

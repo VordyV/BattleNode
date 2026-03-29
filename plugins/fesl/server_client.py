c69bd23584bf83ebceff90ec3cb50f2da51ecf03
vordy <vorklab@outlook.com>
2026-03-29 13:43:46 +0300

    Added property is_profile_authenticated, _remove_profile_id

--- plugins/fesl/server_client.py
+++ plugins/fesl/server_client.py
@@ -2,12 +2,13 @@ from tornado.iostream import StreamClosedError, IOStream
 
 class Client:
 
-    def __init__(self, stream: IOStream, address: tuple[str, str]):
+    def __init__(self, stream: IOStream, address: tuple[str, int]):
         self.__account_id: int | None = None
         self.__stream = stream
         self.__address = address
         self.__is_authenticated: bool = False
         self.__profile_id: int | None = None
+        self.__is_profile_authenticated: bool = False
 
     def _set_account_id(self, ident: int):
         self.__account_id = ident
@@ -15,13 +16,22 @@ class Client:
 
     def _set_profile_id(self, ident: int):
         self.__profile_id = ident
+        self.__is_profile_authenticated = True
+
+    def _remove_profile_id(self):
+        if self.__is_profile_authenticated:
+            self.__profile_id = None
+            self.__is_profile_authenticated = False
+
+    def _set_address(self, address:  tuple[str, int]):
+        self.__address = address
 
     @property
     def stream(self) -> IOStream:
         return self.__stream
 
     @property
-    def address(self) -> tuple[str, str]:
+    def address(self) -> tuple[str, int]:
         return self.__address
 
     @property
@@ -32,6 +42,10 @@ class Client:
     @property
     def is_authenticated(self) -> bool:
         return self.__is_authenticated
+
+    @property
+    def is_profile_authenticated(self) -> bool:
+        return self.__is_profile_authenticated
 
     @property
     def profile_id(self) -> int:

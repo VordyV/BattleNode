00e29020104ff471e42105dc583d6d3d7e337e3b
vordy <vorklab@outlook.com>
2026-05-25 20:30:13 +0300

    Added RoundPlayerStats and RoundPlayers schemas

--- plugins/stats/schemes.py
+++ plugins/stats/schemes.py
@@ -1,6 +1,9 @@
 import pydantic
+import datetime
+from .teams import Teams
+from .game_modes import GameModes
 
-class StatsFieldsScheme(pydantic.BaseModel):
+class StatsScheme(pydantic.BaseModel):
     gsco: int = 0
     crpt: int = 0
     rnk: int = 0
@@ -15,3 +18,18 @@ class StatsFieldsScheme(pydantic.BaseModel):
     bp_1: int = 0
     unavl: int = 0
     klls: int = 0
+
+class RoundPlayerStats(pydantic.BaseModel):
+    pid: int
+    nick: str
+    gsco: int
+    crpt: int
+    rnk: int
+    rnkcg: int
+    tt: int
+    dths: int
+    klls: int
+    team: int
+
+class RoundPlayers(pydantic.BaseModel):
+    players: dict[int, RoundPlayerStats]
